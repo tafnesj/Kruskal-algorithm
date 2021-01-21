6ea79b78b4e589911a644cc8c4a1042ff06edd51
Tafnes Jiménez <55866934+tafnesj@users.noreply.github.com>
2021-01-21 10:59:03 -0600

    Update

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { dirname } = require('path');
 const app = express()
 const port = 3000
+var compression = require('compression');
+var helmet = require('helmet');
 var path = require('path');
 
 app.get('/', function(req, res) {
